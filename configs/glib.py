81c94cbf33c068284f1787b4b9b0f855e48ec9d9
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-08-19 23:44:20 +0900

    Add the configuration file for GLib

---
+++ configs/glib.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+from os.path import expanduser
+
+syntax_name = 'glib'
+file_glob = expanduser('~/src/glib/docs/reference/*/*-decl.txt')
+url = 'https://raw.githubusercontent.com/y-iihoshi/gtk-vim-syntax/master/syntax/glib.vim'
+maintainer = 'IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>'
