5641b9f5045ddb07e0cd64b576e13dfbe68ad430
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-08-23 00:58:51 +0900

    Add the configuration file for dbus-glib

---
+++ configs/dbusglib.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+from os.path import expanduser
+
+syntax_name = 'dbusglib'
+file_glob = expanduser('~/src/dbus-glib/doc/reference/dbus-glib-decl.txt')
+description = 'C dbus-glib extension (for version 0.110)'
+url = 'https://raw.githubusercontent.com/y-iihoshi/gtk-vim-syntax/master/syntax/dbusglib.vim'
+maintainer = 'IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>'
