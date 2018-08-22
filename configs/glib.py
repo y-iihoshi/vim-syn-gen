83b5210b844e6a24752cdd4729809fbbd7762c78
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2018-08-23 00:53:05 +0900

    Describe the version explicitly

--- configs/glib.py
+++ configs/glib.py
@@ -3,5 +3,6 @@ from os.path import expanduser
 
 syntax_name = 'glib'
 file_glob = expanduser('~/src/glib/docs/reference/*/*-decl.txt')
+description = 'C glib extension (for version 2.56.2)'
 url = 'https://raw.githubusercontent.com/y-iihoshi/gtk-vim-syntax/master/syntax/glib.vim'
 maintainer = 'IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>'
